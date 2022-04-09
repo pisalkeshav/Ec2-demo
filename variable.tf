923b811dad3c6feb67da20a06dda6093579ef028
pisalkeshav <102312386+pisalkeshav@users.noreply.github.com>
2022-04-09 20:58:07 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  type    = string
+  default = "i-0171bbff2082cb173"
+
+}
+
+variable "key_name" {
+  type    = string
+  default = "devops2022"
+}
